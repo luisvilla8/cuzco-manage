751fb66af6828ca05e8cf7346436be3385b8c030
Luis Villacorta <85130000+luisvilla8@users.noreply.github.com>
2022-08-22 23:35:39 -0500

    feat:Add hook useLocalStorage

--- src/hook/index.ts
+++ src/hook/index.ts
@@ -1,1 +1,3 @@
+export * from './index';
 export * from './useFetch';
+export * from './useLocalStorage';
---
+++ src/hook/useLocalStorage.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+interface Prop {
+  key: string;
+  initialValue: any;
+}
+
+export const useLocalStorage = (key: string, initialValue: any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+      if( item ) {
+        return JSON.parse(item);
+      }else {
+        localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
+      }
+      // return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  }
+  );
+
+  const setValue = (value: any) => {
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return { storedValue, setValue };
+}
