74b2801c65df263ba1b0fefd3f77966f70cfff44
Luis Villacorta <85130000+luisvilla8@users.noreply.github.com>
2022-12-08 19:49:43 -0500

    fix: removes unused file

--- src/util/modal.ts
+++
@@ -1,5 +0,0 @@
-export const handleBlurModalClose = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-  const target = e.target as HTMLInputElement;
-  const childElementLength = target.childNodes.length;
-  if (childElementLength === 1) handleClose();
-}
